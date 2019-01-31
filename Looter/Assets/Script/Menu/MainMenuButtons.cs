f03de0e1db0213a7c9246048c359e54220a86560
STUDENT\o017861f <o017861f@student.staffs.ac.uk>
2019-01-31 14:54:12 +0000

    Work done on menu, animated panels, planned locations of buttons etc, started art style of menu

--- Looter/Assets/Script/Menu/MainMenuButtons.cs
+++ Looter/Assets/Script/Menu/MainMenuButtons.cs
@@ -5,28 +5,73 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour {
 
+    public GameObject StorePanel;
+    private Animation AnimStore;
+
+    public GameObject AchievePanel;
+    private Animation AnimAchieve;
+
+    public bool SidePanelOpen = false;
+
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
     public void LoadLevel()
     {
-        SceneManager.LoadScene(1);
+        if (!SidePanelOpen)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 
     public void OpenStorePanel()
     {
-        //Pull over store panel from left
+        if (!SidePanelOpen)
+        {
+            //Pull over store panel from left
+            AnimStore = StorePanel.GetComponent<Animation>();
+            AnimStore.Play("OpenStorePanel");
+            SidePanelOpen = true;
+        }
+    }
+    public void CloseStorePanel()
+    {
+        if (SidePanelOpen)
+        {
+            //Push store panel to the right
+            AnimStore = StorePanel.GetComponent<Animation>();
+            AnimStore.Play("CloseStorePanel");
+            SidePanelOpen = false;
+        }
     }
 
     public void OpenAchievementAndQuests()
     {
-        //pull over achievement Panel From right
+        if (!SidePanelOpen)
+        {
+            //pull over achievement Panel From right
+            AnimAchieve = AchievePanel.GetComponent<Animation>();
+            AnimAchieve.Play("OpenAchievePanel");
+            SidePanelOpen = true;
+        }
+    }
+    public void CloseAchievementAndQuests()
+    {
+        if (SidePanelOpen)
+        {
+            //Push achievement Panel to the right
+            AnimAchieve = AchievePanel.GetComponent<Animation>();
+            AnimAchieve.Play("CloseAchievePanel");
+            SidePanelOpen = false;
+        }
     }
 }
